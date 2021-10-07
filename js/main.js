da8d7598d282e3f8804b58ba7b075bade3d40c65
Mathew <bakermg@outlook.com>
2021-10-07 22:01:05 +1100

    Testing Vega Lite Embed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-console.log("script loaded")
+// console.log("script loaded")
 
 
 const switchTo = (id) => {
+    console.log(id)
     $(".centre-section").hide();
     $("#"+id).fadeIn()
 }
 
-$(".link").on("click", switchTo(e))
+// $(".link").on("click", switchTo(e))
+
+// Embed Vega Lite Charts
+vegaEmbed("#death-cause-cc", "death-cause-area.vg.json", {"actions": false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
+
+vegaEmbed("#death-cause-main", "death-cause-area.vg.json", {"actions": false}).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
