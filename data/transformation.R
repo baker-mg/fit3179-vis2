5f410f83ab99625ae78f66119d385c7528163c63
Mathew <bakermg@outlook.com>
2021-10-10 06:50:42 +1100

    Added summit time of day comparison charts.

--- data/transformation.R
+++ data/transformation.R
@@ -2,7 +2,8 @@ library(tidyverse)
 library(tidytuesdayR)
 library(dplyr)
 library(stringr)
-
+library(lubridate)
+library(withr)
 tt <- tt_load("2020-09-22")
 
 
@@ -71,3 +72,47 @@ summit_counts_year <- expeditions %>%
 write.csv(summit_counts_year, "summit_counts_year.csv", row.names = FALSE)
 
 # mutate(month_day = format(as.Date(summit_date), "%m/%d")) %>%
+
+# Time of day
+time_of_day <- expeditions%>%
+  filter(peakid == "EVER", termination_reason == 1, year >= 1990) %>%
+  drop_na(summit_time) %>%
+  mutate(summit_climbers= summit_members+summit_hired) %>%
+  mutate(summit_time = with_options(
+    c(scipen = 999), 
+    str_pad(summit_time, 4, pad = "0")
+  )) %>%
+  mutate(summit_hour = hour(as.POSIXct(strptime(summit_time, "%H%M")))) %>%
+  group_by(year) %>%
+  complete(summit_hour = seq(0, 23, by=1)) %>%
+  group_by(summit_hour, year) %>%
+  summarise(summit_climbers = sum(summit_climbers))
+time_of_day$summit_climbers[is.na(time_of_day$summit_climbers)] <- 0
+
+time_of_day <- time_of_day %>%
+  mutate(range = cut(year, breaks=c(1990, 1999, 2009, 2020), labels = c("1990-1999", "2000-2009", "2010-2020"), include.lowest = TRUE)) %>%
+  group_by(range, summit_hour) %>%
+  summarise(summit_climbers = mean(summit_climbers))
+
+write.csv(time_of_day, "average_time_day.csv")
+
+
+time_of_day <- expeditions%>%
+  filter(peakid == "EVER", termination_reason == 1, year >= 1970) %>%
+  drop_na(summit_time) %>%
+  mutate(summit_climbers= summit_members+summit_hired) %>%
+  mutate(summit_time = with_options(
+    c(scipen = 999), 
+    str_pad(summit_time, 4, pad = "0")
+  )) %>%
+  group_by(summit_time, year) %>%
+  summarise(summit_climbers = sum(summit_climbers))
+
+write.csv(time_of_day, "time-day.csv", row.names = FALSE)
+#  aggregate(summit)
+#count(summit_hour, year)
+#
+#  group_by(summit_hour, year) %>%
+#  summarise(mean_summit_hour = mean(summ))
+#  aggregate(by=list(summit_hour, year), FUN="mean")
+  
